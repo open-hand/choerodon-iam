c38f9e2e52fa514bec589fd7b3ca027e4414f2bf
李宗蔚 <zongwei.li@hand-china.com>
2019-10-26 20:32:55 +0800

    [FIX] 处理release合并后的冲突

--- src/main/java/io/choerodon/base/infra/dto/RouteDTO.java
+++ src/main/java/io/choerodon/base/infra/dto/RouteDTO.java
@@ -1,10 +1,12 @@
 package io.choerodon.base.infra.dto;
 
-import io.choerodon.mybatis.entity.BaseDTO;
+import io.swagger.annotations.ApiModelProperty;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import io.choerodon.mybatis.entity.BaseDTO;
 
 @Table(name = "FD_ROUTE")
 public class RouteDTO extends BaseDTO {
@@ -15,6 +17,14 @@ public class RouteDTO extends BaseDTO {
     private String backendPath;
     private String frontendPath;
 
+    @ApiModelProperty(value = "路由唯一标识/必填")
+    private String routeId;
+
+    @ApiModelProperty(value = "是否去除前缀/非必填")
+    private Boolean stripPrefix;
+
+    @ApiModelProperty(value = "是否过滤敏感头信息/非必填")
+    private String sensitiveHeaders;
 
     public Long getId() {
         return id;
@@ -46,5 +56,29 @@ public class RouteDTO extends BaseDTO {
 
     public void setFrontendPath(String frontendPath) {
         this.frontendPath = frontendPath;
+    }
+
+    public Boolean getStripPrefix() {
+        return stripPrefix;
+    }
+
+    public void setStripPrefix(Boolean stripPrefix) {
+        this.stripPrefix = stripPrefix;
+    }
+
+    public String getSensitiveHeaders() {
+        return sensitiveHeaders;
+    }
+
+    public void setSensitiveHeaders(String sensitiveHeaders) {
+        this.sensitiveHeaders = sensitiveHeaders;
+    }
+
+    public String getRouteId() {
+        return routeId;
+    }
+
+    public void setRouteId(String routeId) {
+        this.routeId = routeId;
     }
 }
--- src/main/resources/script/db/fd_route.groovy
+++ src/main/resources/script/db/fd_route.groovy
@@ -1,17 +1,23 @@
 package script.db
 
-databaseChangeLog(logicalFilePath: 'script/db/fd_service.groovy') {
+databaseChangeLog(logicalFilePath: 'script/db/fd_route.groovy') {
 
     changeSet(author: "xausky", id: "2019-09-10-add-fd-route") {
         createTable(tableName: "FD_ROUTE",remarks: '服务路由表') {
             column(name: 'ID', type: 'BIGINT UNSIGNED', autoIncrement: true, remarks: '表ID，主键，供其他表做外键，unsigned bigint、单表时自增、步长为 1') {
                 constraints(primaryKey: true, primaryKeyName: 'PK_FD_ROUTE')
             }
+            column(name: 'ROUTE_ID', type: 'VARCHAR(64)', remarks: '路由ID') {
+                constraints(nullable: false)
+            }
             column(name: 'SERVICE_CODE', type: 'VARCHAR(64)', remarks: '服务编码') {
                 constraints(nullable: false)
             }
             column(name: "BACKEND_PATH", type: "varchar(240)", remarks: "后端路由")
             column(name: "FRONTEND_PATH", type: "varchar(240)", remarks: "前端路由")
+            column(name: 'STRIP_PREFIX', type: 'INT UNSIGNED', remarks: '是否去前缀')
+            column(name: 'SENSITIVE_HEADERS', type: 'TEXT', remarks: '敏感头部列表')
+
             column(name: "OBJECT_VERSION_NUMBER", type: "BIGINT", defaultValue: "1")
             column(name: "CREATED_BY", type: "bigint", defaultValue: "-1")
             column(name: "CREATION_DATE", type: "datetime", defaultValueComputed: "CURRENT_TIMESTAMP")

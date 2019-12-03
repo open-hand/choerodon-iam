761a04e3df7153a4932b5f4ccccc51546c3a59b9
scp <changping.shi@hand-china.com>
2019-12-03 11:56:58 +0800

    [ADD] 添加ldap自动触发逻辑

--- src/main/java/io/choerodon/base/infra/feign/fallback/AsgardFeignClientFallback.java
+++ src/main/java/io/choerodon/base/infra/feign/fallback/AsgardFeignClientFallback.java
@@ -1,7 +1,14 @@
 package io.choerodon.base.infra.feign.fallback;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
+import io.choerodon.base.infra.dto.asgard.QuartzTask;
+import io.choerodon.base.infra.dto.asgard.ScheduleMethodDTO;
+import io.choerodon.base.infra.dto.asgard.ScheduleTaskDTO;
+import io.choerodon.base.infra.dto.asgard.ScheduleTaskDetail;
 import io.choerodon.core.exception.CommonException;
 import io.choerodon.base.infra.feign.AsgardFeignClient;
 
@@ -18,5 +25,35 @@ public class AsgardFeignClientFallback implements AsgardFeignClient {
     @Override
     public void disableProj(long projectId) {
         throw new CommonException("error.asgard.quartzTask.disableProject");
+    }
+
+    @Override
+    public ResponseEntity<QuartzTask> createOrgTask(long organizationId, ScheduleTaskDTO scheduleTaskDTO) {
+        throw new CommonException("error.asgard.quartzTask.createOrganization");
+    }
+
+    @Override
+    public void deleteOrgTask(long orgId, long id) {
+        throw new CommonException("error.asgard.quartzTask.deleteOrganization");
+    }
+
+    @Override
+    public void enableOrgTask(long orgId, long id, long objectVersionNumber) {
+        throw new CommonException("error.asgard.quartzTask.enableOrganization");
+    }
+
+    @Override
+    public ResponseEntity<List<ScheduleMethodDTO>> getMethodByService(long orgId, String service) {
+        throw new CommonException("error.asgard.method.query");
+    }
+
+    @Override
+    public void disableOrgTask(long orgId, long id, long objectVersionNumber) {
+        throw new CommonException("error.asgard.orgTask.disable");
+    }
+
+    @Override
+    public ResponseEntity<ScheduleTaskDetail> getTaskDetail(long orgId, long id) {
+        throw new CommonException("error.asgard.task.detail");
     }
 }
---
+++ src/main/java/io/choerodon/base/infra/mapper/LdapAutoMapper.java
@@ -0,0 +1,8 @@
+package io.choerodon.base.infra.mapper;
+
+import io.choerodon.base.infra.dto.LdapAutoDTO;
+import io.choerodon.mybatis.common.Mapper;
+
+public interface LdapAutoMapper extends Mapper<LdapAutoDTO> {
+}
+

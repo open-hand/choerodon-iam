86dbdfffca207c0b957218fda76ac8553c535543
wangxiang <xiangwang04@hand-china.com>
2020-02-26 18:20:56 +0800

    [FIX]修改接口

--- src/main/java/io/choerodon/base/api/controller/v1/OperateLogController.java
+++ src/main/java/io/choerodon/base/api/controller/v1/OperateLogController.java
@@ -1,18 +1,24 @@
 package io.choerodon.base.api.controller.v1;
 
+import com.github.pagehelper.PageInfo;
 import io.choerodon.base.api.vo.OperateLogVO;
 import io.choerodon.base.app.service.OperateLogService;
 import io.choerodon.core.annotation.Permission;
 import io.choerodon.core.enums.ResourceType;
 import io.choerodon.core.iam.InitRoleCode;
+import io.choerodon.swagger.annotation.CustomPageRequest;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.SortDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 import java.util.List;
 
@@ -26,19 +32,15 @@ public class OperateLogController {
     @Autowired
     private OperateLogService operateLogService;
 
-    @GetMapping("/new/operate/log")
-    @ApiOperation("概览8条最新的操作记录")
-    @Permission(type = ResourceType.ORGANIZATION, roles = {InitRoleCode.ORGANIZATION_ADMINISTRATOR, InitRoleCode.SITE_ADMINISTRATOR})
-    public ResponseEntity<List<OperateLogVO>> listNewOperateLog(
-            @PathVariable(value = "source_id") Long sourceId) {
-        return new ResponseEntity<>(operateLogService.listNewOperateLog(sourceId), HttpStatus.OK);
-    }
 
-    @GetMapping("/more/operate/log")
-    @ApiOperation("加载更多100条操作记录")
+    @GetMapping("/operate/log")
+    @ApiOperation("加载更多操作记录")
     @Permission(type = ResourceType.ORGANIZATION, roles = {InitRoleCode.ORGANIZATION_ADMINISTRATOR, InitRoleCode.SITE_ADMINISTRATOR})
-    public ResponseEntity<List<OperateLogVO>> listMoreOperateLog(
-            @PathVariable(value = "source_id") Long sourceId) {
-        return new ResponseEntity<>(operateLogService.listMoreOperateLog(sourceId), HttpStatus.OK);
+    @CustomPageRequest
+    public ResponseEntity<PageInfo<OperateLogVO>> listMoreOperateLog(
+            @PathVariable(value = "source_id") Long sourceId,
+            @ApiIgnore
+            @SortDefault(value = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+        return new ResponseEntity<>(operateLogService.listOperateLog(pageable,sourceId), HttpStatus.OK);
     }
 }
--- src/main/java/io/choerodon/base/app/service/OperateLogService.java
+++ src/main/java/io/choerodon/base/app/service/OperateLogService.java
@@ -1,15 +1,14 @@
 package io.choerodon.base.app.service;
 
+import com.github.pagehelper.PageInfo;
 import io.choerodon.base.api.vo.OperateLogVO;
+import org.springframework.data.domain.Pageable;
 
-import java.util.List;
 
 /**
  * User: Mr.Wang
  * Date: 2020/2/25
  */
 public interface OperateLogService {
-    List<OperateLogVO> listNewOperateLog(Long sourceId);
-
-    List<OperateLogVO>  listMoreOperateLog(Long sourceId);
+    PageInfo<OperateLogVO> listOperateLog(Pageable pageable, Long sourceId);
 }

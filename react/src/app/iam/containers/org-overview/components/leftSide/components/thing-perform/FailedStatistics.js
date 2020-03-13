acdc1dd9fa917206f9cf36b38ba867602a8bd7c6
NollieLeo <76653759@qq.com>
2020-03-13 10:36:52 +0800

    [IMP] 优化概览事务统计失败列表文字颜色

--- react/src/app/iam/containers/org-overview/components/leftSide/components/thing-perform/FailedStatistics.js
+++ react/src/app/iam/containers/org-overview/components/leftSide/components/thing-perform/FailedStatistics.js
@@ -27,6 +27,14 @@ const FailedStatistics = withRouter(observer((props) => {
     <a className="c7n-overview-sage" onClick={() => handleClickSagaRecord(record)}>{`${value}-${record.get('id')}`}</a>
   );
 
+  const renderRefType = ({ value, record }) => (
+    <span style={{ color: 'rgba(0,0,0,.65)' }}>{value}</span>
+  );
+
+  const renderStartTime = ({ value, record }) => (
+    <span style={{ color: 'rgba(0,0,0,.65)' }}>{value}</span>
+  );
+
   return (
     <div className="c7n-overview-failedStatistics">
       <span className="c7n-overview-failedStatistics-titlespan">失败事务统计</span>
@@ -37,8 +45,8 @@ const FailedStatistics = withRouter(observer((props) => {
       >
 
         <Column name="sagaCode" renderer={renderSagaCode} />
-        <Column name="refType" />
-        <Column name="startTime" />
+        <Column name="refType" renderer={renderRefType} />
+        <Column name="startTime" renderer={renderStartTime} />
       </Table>
     </div>
   );

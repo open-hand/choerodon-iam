c6f5c4781ab5621fee0597527cd3bc0226a8a456
zmf <1984654893@qq.com>
2020-03-18 12:54:58 +0800

    [ADD] add API to query user with role and gitlabUserId by ids

--- src/main/java/io/choerodon/base/app/service/UserService.java
+++ src/main/java/io/choerodon/base/app/service/UserService.java
@@ -175,6 +175,14 @@ public interface UserService {
     PageInfo<UserDTO> pagingQueryUsersWithRolesOnProjectLevel(Long projectId, Pageable Pageable, String loginName, String realName,
                                                               String roleName, Boolean enabled, String params);
 
+
+    /**
+     * 项目层分页查询用户列表（包括用户信息以及所分配的项目角色信息）.
+     *
+     * @return 用户列表（包括用户信息以及所分配的项目角色信息）
+     */
+    List<UserWithGitlabIdDTO> listUsersWithRolesAndGitlabUserIdByIds(Long projectId, Set<Long> userIds);
+
     /**
      * 在全局层/组织层/项目层 批量分配给用户角色.
      *

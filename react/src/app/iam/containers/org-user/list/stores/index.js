fe4c092b79c62db7907374dc402f37037c4885f0
20399 <yanfang.lin@hand-china.com>
2020-03-27 17:11:36 +0800

    [IMP] excel中添加判断能否创建用户接口权限

--- react/src/app/iam/containers/org-user/list/stores/index.js
+++ react/src/app/iam/containers/org-user/list/stores/index.js
@@ -44,7 +44,7 @@ export const StoreProvider = injectIntl(inject('AppState')(
 
     useEffect(() => {
       userStore.checkCreate(organizationId);
-    }, []);
+    }, [organizationId]);
     
     const value = {
       ...props,

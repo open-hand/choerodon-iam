7d6a9e9b9230b3c026f966a2abdd93dfd18e0704
zmf <1984654893@qq.com>
2020-04-23 10:28:34 +0800

    [FIX] fix sth

--- src/main/java/io/choerodon/iam/infra/feign/fallback/TestManagerFeignClientFallback.java
+++ src/main/java/io/choerodon/iam/infra/feign/fallback/TestManagerFeignClientFallback.java
@@ -3,8 +3,6 @@ package io.choerodon.iam.infra.feign.fallback;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
-import io.choerodon.base.api.vo.AgileProjectInfoVO;
-import io.choerodon.base.infra.feign.TestManagerFeignClient;
 import io.choerodon.core.exception.CommonException;
 import io.choerodon.iam.api.vo.AgileProjectInfoVO;
 import io.choerodon.iam.infra.feign.TestManagerFeignClient;

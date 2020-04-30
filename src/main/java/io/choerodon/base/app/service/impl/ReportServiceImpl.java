81da15a52ede1c2f274d1f9ffd4878889ed44d0c
王灏 <2507172753@qq.com>
2020-04-30 13:52:56 +0800

    [ADD]运维项目类型 图表过滤

--- src/main/java/io/choerodon/base/app/service/impl/ReportServiceImpl.java
+++ src/main/java/io/choerodon/base/app/service/impl/ReportServiceImpl.java
@@ -3,6 +3,7 @@ package io.choerodon.base.app.service.impl;
 import io.choerodon.base.api.dto.ReportDTO;
 import io.choerodon.base.app.service.ReportService;
 import io.choerodon.base.infra.dto.ProjectDTO;
+import io.choerodon.base.infra.enums.ProjectCategoryEnum;
 import io.choerodon.base.infra.mapper.ProjectMapper;
 import io.choerodon.base.infra.mapper.ReportMapper;
 import org.springframework.stereotype.Service;
@@ -34,6 +35,11 @@ public class ReportServiceImpl implements ReportService {
             return reportDTOS.stream()
                     .filter(reportDTO -> reportDTO.getReportType().equals("敏捷报表")
                             || reportDTO.getReportType().equals("测试报表"))
+                    .collect(Collectors.toList());
+        }
+        if(projectDTO.getCategories().stream().anyMatch(categoryDTO -> ProjectCategoryEnum.OPERATIONS.value().equals(categoryDTO.getCode()))) {
+            return reportDTOS.stream()
+                    .filter(reportDTO -> reportDTO.getReportType().equals("DevOps 报表"))
                     .collect(Collectors.toList());
         }
         return reportDTOS;

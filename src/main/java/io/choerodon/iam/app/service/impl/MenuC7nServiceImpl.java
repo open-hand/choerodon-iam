f5f2a63565aa346ac25f08506fd3092ee7eaaa68
scp <changping.shi@hand-china.com>
2020-05-08 17:50:37 +0800

    [IMP] 添加菜单查询接口

--- src/main/java/io/choerodon/iam/app/service/impl/MenuC7nServiceImpl.java
+++ src/main/java/io/choerodon/iam/app/service/impl/MenuC7nServiceImpl.java
@@ -1,9 +1,11 @@
 package io.choerodon.iam.app.service.impl;
 
 import java.util.List;
+import java.util.Set;
 
 import org.hzero.iam.domain.entity.Menu;
 import org.hzero.iam.domain.entity.Role;
+import org.hzero.iam.domain.repository.MenuRepository;
 import org.hzero.iam.domain.repository.RoleRepository;
 import org.hzero.iam.infra.constant.LabelConstants;
 import org.springframework.stereotype.Service;
@@ -22,14 +24,19 @@ import io.choerodon.iam.infra.mapper.MenuC7nMapper;
 @Service
 public class MenuC7nServiceImpl implements MenuC7nService {
 
+    private static String DEFAULT_LANG="zh_CN";
+
     private MenuC7nMapper menuC7nMapper;
     private OrganizationRoleC7nService organizationRoleC7nService;
     private RoleRepository roleRepository;
+    private MenuRepository menuRepository;
 
     public MenuC7nServiceImpl(MenuC7nMapper menuC7nMapper,
                               OrganizationRoleC7nService organizationRoleC7nService,
+                              MenuRepository menuRepository,
                               RoleRepository roleRepository) {
         this.menuC7nMapper = menuC7nMapper;
+        this.menuRepository = menuRepository;
         this.organizationRoleC7nService = organizationRoleC7nService;
         this.roleRepository = roleRepository;
     }
@@ -41,5 +48,11 @@ public class MenuC7nServiceImpl implements MenuC7nService {
 
         // 根据层级查询组织管理员的有权限的菜单列表
         return roleRepository.selectRolePermissionSetTree(orgAdmin.getId(), null);
+    }
+
+    @Override
+    public List<Menu> listNavMenuTree(Long roleId, Set<String> labels, Long projectId) {
+        List<Menu> menuList = menuRepository.selectRoleMenuTree(roleId, DEFAULT_LANG, labels);
+        return null;
     }
 }

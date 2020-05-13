690ac8d6038af20415a159026439ba2ded2573a6
lihao <310103270@qq.com>
2020-05-13 14:53:49 +0800

    [ADD] 添加【敏捷专用】根据组织id获取时区工作日历 接口

---
+++ src/main/java/io/choerodon/iam/app/service/impl/TimeZoneWorkCalendarServiceImpl.java
@@ -0,0 +1,40 @@
+package io.choerodon.iam.app.service.impl;
+
+import io.choerodon.iam.app.service.TimeZoneWorkCalendarService;
+import io.choerodon.iam.infra.dto.TimeZoneWorkCalendarDTO;
+import io.choerodon.iam.infra.mapper.TimeZoneWorkCalendarMapper;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.PostConstruct;
+
+
+/**
+ * @author dinghuang123@gmail.com
+ * @since 2018/10/12
+ */
+@Component
+@Transactional(rollbackFor = Exception.class)
+public class TimeZoneWorkCalendarServiceImpl implements TimeZoneWorkCalendarService {
+
+    @Autowired
+    private TimeZoneWorkCalendarMapper timeZoneWorkCalendarMapper;
+
+    private ModelMapper modelMapper = new ModelMapper();
+
+    @PostConstruct
+    public void init() {
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+    }
+
+
+    @Override
+    public TimeZoneWorkCalendarDTO queryTimeZoneDetailByOrganizationId(Long organizationId) {
+        return timeZoneWorkCalendarMapper.queryTimeZoneDetailByOrganizationId(organizationId);
+    }
+}

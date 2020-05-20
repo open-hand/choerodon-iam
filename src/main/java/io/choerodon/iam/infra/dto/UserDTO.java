79872d7ac7d73b4ea2fba6126eaea677e3026389
zmf <1984654893@qq.com>
2020-05-20 09:24:32 +0800

    [ADD] add @Transient

--- src/main/java/io/choerodon/iam/infra/dto/UserDTO.java
+++ src/main/java/io/choerodon/iam/infra/dto/UserDTO.java
@@ -16,6 +16,7 @@ public class UserDTO extends User {
 
     public static final String PHONE_REG = "^[1](([3][0-9])|([4][5-9])|([5][0-3,5-9])|([6][5,6])|([7][0-8])|([8][0-9])|([9][1,8,9]))[0-9]{8}$";
 
+    @Transient
     @ApiModelProperty("组织code")
     private String organizationCode;
 

7fbc639501c85a15d091aa669b920d217d4a2f2e
zmf <1984654893@qq.com>
2020-05-26 16:38:13 +0800

    [ADD] add execl-executor configuration

--- src/main/java/io/choerodon/iam/IamApplication.java
+++ src/main/java/io/choerodon/iam/IamApplication.java
@@ -1,10 +1,14 @@
 package io.choerodon.iam;
 
 import org.hzero.autoconfigure.iam.EnableHZeroIam;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @EnableFeignClients("io.choerodon.iam")
 @EnableHZeroIam
@@ -15,5 +19,14 @@ public class IamApplication {
     public static void main(String[] args) {
         SpringApplication.run(IamApplication.class, args);
     }
-    
+
+    @Bean
+    @Qualifier("excel-executor")
+    public AsyncTaskExecutor excelImportUserExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setThreadNamePrefix("excel-executor");
+        executor.setMaxPoolSize(5);
+        executor.setCorePoolSize(4);
+        return executor;
+    }
 } 

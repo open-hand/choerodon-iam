638d510653903c55c71a86f39f9d9b8e85a5ba64
lihao <310103270@qq.com>
2020-05-26 10:05:13 +0800

    [IMP] 去掉操作记录查询接口

--- src/main/java/io/choerodon/iam/app/service/impl/OperateLogServiceImpl.java
+++ src/main/java/io/choerodon/iam/app/service/impl/OperateLogServiceImpl.java
@@ -1,14 +1,7 @@
 package io.choerodon.iam.app.service.impl;
 
-import io.choerodon.core.domain.Page;
-import io.choerodon.core.iam.ResourceLevel;
-import io.choerodon.iam.api.vo.OperateLogVO;
 import io.choerodon.iam.app.service.OperateLogService;
-import io.choerodon.iam.infra.annotation.OperateLog;
 import io.choerodon.iam.infra.feign.AsgardFeignClient;
-import io.choerodon.iam.infra.mapper.OperateLogMapper;
-import io.choerodon.mybatis.pagehelper.PageHelper;
-import io.choerodon.mybatis.pagehelper.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,32 +10,21 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class OperateLogServiceImpl implements OperateLogService {
-    private OperateLogMapper operateLogMapper;
     private AsgardFeignClient asgardFeignClient;
 
-    public OperateLogServiceImpl(OperateLogMapper operateLogMapper, AsgardFeignClient asgardFeignClient) {
-        this.operateLogMapper = operateLogMapper;
+    public OperateLogServiceImpl(AsgardFeignClient asgardFeignClient) {
         this.asgardFeignClient = asgardFeignClient;
     }
 
 
     @Override
-    public Page<OperateLogVO> listOperateLog(PageRequest pageRequest, Long sourceId) {
-        if (sourceId == 0L) {
-            return PageHelper.doPageAndSort(pageRequest, () -> operateLogMapper.listOperateLogSite());
-
-        }
-        return PageHelper.doPageAndSort(pageRequest, () -> operateLogMapper.listOperateLogOrg(sourceId));
-    }
-
-    @Override
-    @OperateLog(type = "siteRetry", content = "%s重试了事务实例“%s”", level = {ResourceLevel.SITE})
+//    @OperateLog(type = "siteRetry", content = "%s重试了事务实例“%s”", level = {ResourceLevel.SITE})
     public void siteRetry(Long sourceId, long id) {
         asgardFeignClient.retry(id);
     }
 
     @Override
-    @OperateLog(type = "orgRetry", content = "%s重试了事务实例“%s”", level = {ResourceLevel.ORGANIZATION})
+//    @OperateLog(type = "orgRetry", content = "%s重试了事务实例“%s”", level = {ResourceLevel.ORGANIZATION})
     public void orgRetry(Long sourceId, long id) {
         asgardFeignClient.retry(id);
     }

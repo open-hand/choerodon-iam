f814b551c0114c8f70033a7a82d1ac21967ff6d6
scp <changping.shi@hand-china.com>
2020-06-12 14:02:46 +0800

    [IMP] 修改循环依赖问题

--- src/main/java/io/choerodon/iam/app/service/impl/UserC7nServiceImpl.java
+++ src/main/java/io/choerodon/iam/app/service/impl/UserC7nServiceImpl.java
@@ -64,6 +64,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.aop.framework.AopContext;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -122,6 +123,7 @@ public class UserC7nServiceImpl implements UserC7nService {
     @Autowired
     private MemberRoleService memberRoleService;
     @Autowired
+    @Lazy
     private RoleMemberService roleMemberService;
     @Autowired
     private OrganizationUserService organizationUserService;

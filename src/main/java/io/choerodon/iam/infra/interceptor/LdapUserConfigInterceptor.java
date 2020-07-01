145077f82e1f29909902486a56e8a9e106429d87
scp <changping.shi@hand-china.com>
2020-07-01 15:27:02 +0800

    [FIX] 修复ldap同步用户不创建gitlab用户

---
+++ src/main/java/io/choerodon/iam/infra/interceptor/LdapUserConfigInterceptor.java
@@ -0,0 +1,38 @@
+package io.choerodon.iam.infra.interceptor;
+
+import org.hzero.core.interceptor.HandlerInterceptor;
+import org.hzero.iam.domain.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import io.choerodon.core.iam.ResourceLevel;
+import io.choerodon.iam.app.service.OrganizationUserService;
+
+/**
+ * @author scp
+ * @date 2020/7/1
+ * @description
+ */
+@Component
+public class LdapUserConfigInterceptor implements HandlerInterceptor<User> {
+
+    @Autowired
+    private OrganizationUserService organizationUserService;
+
+
+    public LdapUserConfigInterceptor() {
+    }
+
+    public String name() {
+        return "USER_CONFIG_LDAP";
+    }
+
+    @Override
+    public void interceptor(User user) {
+        if (user.getLdap() == null || !user.getLdap()) {
+            return;
+        }
+        organizationUserService.sendUserCreationSaga(null, user, null, ResourceLevel.ORGANIZATION.value(), user.getOrganizationId());
+    }
+}
+

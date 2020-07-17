2a7c2905d884a1579d48d003ec629334b786ea15
20399 <yanfang.lin@hand-china.com>
2020-07-17 14:40:41 +0800

    [IMP] 修改ldap自动同步中id数据类型

--- react/src/app/iam/containers/org-user/list/ldapModal/stores/SyncRecordDataSet.js
+++ react/src/app/iam/containers/org-user/list/ldapModal/stores/SyncRecordDataSet.js
@@ -13,6 +13,6 @@ export default ({ orgId, ldapId }) => ({
     { name: 'errorUserCount', label: '同步用户失败的数量', type: 'number', defaultValue: 0 },
     { name: 'syncBeginTime', label: '上次同步的时间', type: 'string' },
     { name: 'syncEndTime', label: '同步结束时间', type: 'string' },
-    { name: 'ldapId', label: '同步id', type: 'number' },
+    { name: 'ldapId', label: '同步id', type: 'string' },
   ],
 });

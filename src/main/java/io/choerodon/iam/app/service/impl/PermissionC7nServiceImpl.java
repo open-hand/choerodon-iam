61a2d2a7dae895da5f9d048186b30950f1a399ca
王灏 <2507172753@qq.com>
2020-08-02 22:03:50 +0800

    [imp]添加修复角色权限接口
    (cherry picked from commit b1b540288ca9e73f398589c5f382c844e7f3c6c0)

--- src/main/java/io/choerodon/iam/app/service/impl/PermissionC7nServiceImpl.java
+++ src/main/java/io/choerodon/iam/app/service/impl/PermissionC7nServiceImpl.java
@@ -17,7 +17,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 

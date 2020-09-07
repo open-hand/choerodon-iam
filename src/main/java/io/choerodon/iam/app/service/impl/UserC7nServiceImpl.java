74bc6c570d29cab221db4ddf0a88a0d93137d382
wangxiang <xiangwang04@hand-china.com>
2020-09-07 16:16:12 +0800

    [FIX] 修复更新用户信息的问题

--- src/main/java/io/choerodon/iam/app/service/impl/UserC7nServiceImpl.java
+++ src/main/java/io/choerodon/iam/app/service/impl/UserC7nServiceImpl.java
@@ -174,22 +174,21 @@ public class UserC7nServiceImpl implements UserC7nService {
     }
 
     @Override
-    @Transactional
+    @Transactional(rollbackFor = Exception.class)
     public User updateInfo(User user, Boolean checkLogin) {
         if (checkLogin) {
             checkLoginUser(user.getId());
         }
         User dto;
         UserEventPayload userEventPayload = new UserEventPayload();
-        dto = userService.updateUser(user);
-
-        // hzero update 不更新imageUrl
-        User imageUser = new User();
-        imageUser.setId(dto.getId());
-        imageUser.setImageUrl(user.getImageUrl());
-        imageUser.setObjectVersionNumber(dto.getObjectVersionNumber());
-        userMapper.updateByPrimaryKeySelective(imageUser);
-
+        //跟新固定的字段
+        userRepository.updateOptional(user,
+                User.FIELD_REAL_NAME,
+                User.FIELD_EMAIL,
+                User.FIELD_PHONE,
+                User.DEFAULT_LANGUAGE,
+                User.DEFAULT_TIME_ZONE);
+        dto = userRepository.selectByPrimaryKey(user.getId());
         userEventPayload.setEmail(dto.getEmail());
         userEventPayload.setId(dto.getId().toString());
         userEventPayload.setName(dto.getRealName());

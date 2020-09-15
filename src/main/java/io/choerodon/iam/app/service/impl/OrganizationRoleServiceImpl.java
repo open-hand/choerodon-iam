a9980f28e308d57909866e944bfdf76a3e17b96d
scp <changping.shi@hand-china.com>
2020-09-15 15:57:14 +0800

    [IMP] 修改角色创建

--- src/main/java/io/choerodon/iam/app/service/impl/OrganizationRoleServiceImpl.java
+++ src/main/java/io/choerodon/iam/app/service/impl/OrganizationRoleServiceImpl.java
@@ -8,7 +8,8 @@ import java.util.stream.Collectors;
 
 import org.hzero.iam.app.service.RoleService;
 import org.hzero.iam.domain.entity.*;
-//import org.hzero.iam.domain.service.role.impl.RoleCreateInternalService;
+import org.hzero.iam.domain.service.role.RoleCreateService;
+import org.hzero.iam.domain.service.role.validator.InternalRoleCreateValidator;
 import org.hzero.iam.infra.common.utils.UserUtils;
 import org.hzero.iam.infra.constant.HiamMenuType;
 import org.hzero.iam.infra.constant.RolePermissionType;
@@ -49,9 +50,6 @@ public class OrganizationRoleServiceImpl implements OrganizationRoleC7nService {
     private static final String ERROR_BUILT_IN_ROLE_NOT_BE_EDIT = "error.built.in.role.not.be.edit";
     private static final String ERROR_ROLE_ID_NOT_BE_NULL = "error.role.id.not.be.null";
     private static final String DELETE_ENABLED_ROLE_FAILED = "delete.enabled.role.failed";
-    // todo scp hzero没有该类了
-//    @Autowired
-//    private RoleCreateInternalService roleCreateInternalService;
     @Autowired
     private RoleService roleService;
     @Autowired
@@ -72,6 +70,10 @@ public class OrganizationRoleServiceImpl implements OrganizationRoleC7nService {
     private MemberRoleMapper memberRoleMapper;
     @Autowired
     private LabelRelMapper labelRelMapper;
+    @Autowired
+    private RoleCreateService roleCreateService;
+    @Autowired
+    private InternalRoleCreateValidator internalRoleCreateValidator;
 
     @Override
     @Transactional
@@ -93,16 +95,16 @@ public class OrganizationRoleServiceImpl implements OrganizationRoleC7nService {
         User adminUser = new User();
         adminUser.setId(details.getUserId());
         roleVO.setTenantId(organizationId);
-//        Role role = roleCreateInternalService.createRole(roleVO, adminUser, false, false);
+        Role role = roleCreateService.createRole(roleVO, adminUser, false, false, internalRoleCreateValidator);
 
-//
-//        // 分配权限集
-//        // 默认分配个人信息权限集
-//
-//        Set<Long> psIds = listUserInfoPsIds();
-//
-//        roleVO.getMenuIdList().addAll(psIds);
-//        assignRolePermission(role.getId(), roleVO.getMenuIdList());
+
+        // 分配权限集
+        // 默认分配个人信息权限集
+
+        Set<Long> psIds = listUserInfoPsIds();
+
+        roleVO.getMenuIdList().addAll(psIds);
+        assignRolePermission(role.getId(), roleVO.getMenuIdList());
     }
 
     private Set<Long> listUserInfoPsIds() {

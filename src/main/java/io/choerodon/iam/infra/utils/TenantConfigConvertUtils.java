1e531fc38645f7c1c0ed5dd02dcad710a6a02b0c
王灏 <2507172753@qq.com>
2020-10-14 16:59:37 +0800

    [imp]修改修改组织管理分页查询

--- src/main/java/io/choerodon/iam/infra/utils/TenantConfigConvertUtils.java
+++ src/main/java/io/choerodon/iam/infra/utils/TenantConfigConvertUtils.java
@@ -7,6 +7,8 @@ import java.util.Objects;
 
 
 import org.hzero.iam.domain.entity.TenantConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import io.choerodon.iam.api.vo.TenantConfigVO;
 import io.choerodon.iam.infra.enums.TenantConfigEnum;
@@ -15,43 +17,49 @@ public class TenantConfigConvertUtils {
 
     private static final Integer CONSTANT_ONE = 1;
     private static final String DEFAULT = "DEFAULT";
+    private static final Logger LOGGER = LoggerFactory.getLogger(TenantConfigConvertUtils.class);
+
 
     public static TenantConfigVO configDTOToVO(List<TenantConfig> configs) {
         TenantConfigVO tenantConfigVO = new TenantConfigVO();
-        configs.forEach(t -> {
-            switch (TenantConfigEnum.forValue(t.getConfigKey())) {
-                case ADDRESS:
-                    tenantConfigVO.setAddress(t.getConfigValue());
-                    break;
-                case SCALE:
-                    tenantConfigVO.setScale(t.getConfigValue());
-                    break;
-                case HOME_PAGE:
-                    tenantConfigVO.setHomePage(t.getConfigValue());
-                    break;
-                case IMAGE_URL:
-                    tenantConfigVO.setImageUrl(t.getConfigValue());
-                    break;
-                case BUSINESS_TYPE:
-                    tenantConfigVO.setBusinessType(t.getConfigValue());
-                    break;
-                case EMAIL_SUFFIX:
-                    tenantConfigVO.setEmailSuffix(t.getConfigValue());
-                    break;
-                case IS_REGISTER:
-                    tenantConfigVO.setRegister(Boolean.valueOf(t.getConfigValue()));
-                    break;
-                case USER_ID:
-                    tenantConfigVO.setUserId(Long.parseLong(t.getConfigValue()));
-                    break;
-                case REMOTE_TOKEN_ENABLED:
-                    tenantConfigVO.setRemoteTokenEnabled(Boolean.valueOf(t.getConfigValue()));
-                    break;
-                case CATEGORY:
-                    tenantConfigVO.setCategory(t.getConfigValue());
-                    break;
-            }
-        });
+        try {
+            configs.forEach(t -> {
+                switch (TenantConfigEnum.forValue(t.getConfigKey())) {
+                    case ADDRESS:
+                        tenantConfigVO.setAddress(t.getConfigValue());
+                        break;
+                    case SCALE:
+                        tenantConfigVO.setScale(t.getConfigValue());
+                        break;
+                    case HOME_PAGE:
+                        tenantConfigVO.setHomePage(t.getConfigValue());
+                        break;
+                    case IMAGE_URL:
+                        tenantConfigVO.setImageUrl(t.getConfigValue());
+                        break;
+                    case BUSINESS_TYPE:
+                        tenantConfigVO.setBusinessType(t.getConfigValue());
+                        break;
+                    case EMAIL_SUFFIX:
+                        tenantConfigVO.setEmailSuffix(t.getConfigValue());
+                        break;
+                    case IS_REGISTER:
+                        tenantConfigVO.setRegister(Boolean.valueOf(t.getConfigValue()));
+                        break;
+                    case USER_ID:
+                        tenantConfigVO.setUserId(Long.parseLong(t.getConfigValue()));
+                        break;
+                    case REMOTE_TOKEN_ENABLED:
+                        tenantConfigVO.setRemoteTokenEnabled(Boolean.valueOf(t.getConfigValue()));
+                        break;
+                    case CATEGORY:
+                        tenantConfigVO.setCategory(t.getConfigValue());
+                        break;
+                }
+            });
+        } catch (Exception e) {
+            LOGGER.warn("Compatible dirty data:{}",e.getMessage());
+        }
         return tenantConfigVO;
     }
 

b2e7ff268c6c5500f235e0cd7e2e1cd28463cd07
lihao <310103270@qq.com>
2020-10-19 17:23:27 +0800

    [IMP] 添加项目是否标记为星标项目字段

--- src/main/java/io/choerodon/iam/app/service/StarProjectService.java
+++ src/main/java/io/choerodon/iam/app/service/StarProjectService.java
@@ -1,9 +1,9 @@
 package io.choerodon.iam.app.service;
 
+import java.util.List;
+
 import io.choerodon.iam.infra.dto.ProjectDTO;
 import io.choerodon.iam.infra.dto.StarProjectUserRelDTO;
-
-import java.util.List;
 
 /**
  * 〈功能简述〉
@@ -32,4 +32,12 @@ public interface StarProjectService {
     List<ProjectDTO> query(Long organizationId, Integer size);
 
     void updateStarProject(Long organizationId, List<ProjectDTO> projectDTOS);
+
+    /**
+     * 查询项目是否为星标项目
+     *
+     * @param projectId 项目id
+     * @return boolean
+     */
+    boolean isStarProject(Long projectId);
 }

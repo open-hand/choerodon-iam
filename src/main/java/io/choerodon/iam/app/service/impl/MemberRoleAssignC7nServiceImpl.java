1178959f2abff578b0250faaeec766cd2064c534
devil-scp <changping.shi@hand-china.com>
2020-11-05 22:44:11 +0800

    [IMP] 修改分角色限制

--- src/main/java/io/choerodon/iam/app/service/impl/MemberRoleAssignC7nServiceImpl.java
+++ src/main/java/io/choerodon/iam/app/service/impl/MemberRoleAssignC7nServiceImpl.java
@@ -1,6 +1,7 @@
 package io.choerodon.iam.app.service.impl;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.hzero.iam.domain.entity.Label;
@@ -13,6 +14,7 @@ import org.springframework.util.CollectionUtils;
 import org.springframework.util.ObjectUtils;
 
 import io.choerodon.core.exception.CommonException;
+import io.choerodon.core.iam.ResourceLevel;
 import io.choerodon.iam.infra.constant.MemberRoleConstants;
 import io.choerodon.iam.infra.enums.MemberType;
 import io.choerodon.iam.infra.enums.RoleLabelEnum;
@@ -34,14 +36,20 @@ public class MemberRoleAssignC7nServiceImpl extends MemberRoleAssignService {
      * @param memberRoleList 成员角色
      */
     protected void checkValidityAndInit(List<MemberRole> memberRoleList) {
-        for (MemberRole memberRole : memberRoleList) {
-            List<String> labelList = roleC7nMapper.listRoleLabels(memberRole.getRoleId()).stream().map(Label::getName).collect(Collectors.toList());
-            if (labelList.contains(RoleLabelEnum.PROJECT_ROLE.value())
-                    && (memberRole.getMemberType().equals(MemberType.USER.value()))
-                    && (CollectionUtils.isEmpty(memberRole.getAdditionalParams())
-                    || ObjectUtils.isEmpty(memberRole.getAdditionalParams().get(MemberRoleConstants.MEMBER_TYPE))
-                    || !MemberRoleConstants.MEMBER_TYPE_CHOERODON.equals(memberRole.getAdditionalParams().get(MemberRoleConstants.MEMBER_TYPE)))) {
-                throw new CommonException("error.role.type");
+        Map<Long, List<MemberRole>> listMap = memberRoleList.stream().collect(Collectors.groupingBy(MemberRole::getMemberId));
+        for (Long memberId : listMap.keySet()) {
+            List<Long> oldTenantRoleIds = memberRoleC7nMapper.listRoleByUserIdAndLevel(memberId, ResourceLevel.ORGANIZATION.value()).stream().map(Role::getId).collect(Collectors.toList());
+            for (MemberRole memberRole : listMap.get(memberId)) {
+                if (oldTenantRoleIds.contains(memberRole.getRoleId())) {
+                    return;
+                }
+                List<String> labelList = roleC7nMapper.listRoleLabels(memberRole.getRoleId()).stream().map(Label::getName).collect(Collectors.toList());
+                if (labelList.contains(RoleLabelEnum.PROJECT_ROLE.value())
+                        && (CollectionUtils.isEmpty(memberRole.getAdditionalParams())
+                        || ObjectUtils.isEmpty(memberRole.getAdditionalParams().get(MemberRoleConstants.MEMBER_TYPE))
+                        || !MemberRoleConstants.MEMBER_TYPE_CHOERODON.equals(memberRole.getAdditionalParams().get(MemberRoleConstants.MEMBER_TYPE)))) {
+                    throw new CommonException("error.role.type");
+                }
             }
         }
         super.checkValidityAndInit(memberRoleList);

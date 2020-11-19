b35b9f24b5b3d56688355d6a4e5cb2bb8e084967
devil-scp <changping.shi@hand-china.com>
2020-11-20 00:17:42 +0800

    [FIX] 修复限制在hzero界面处理项目层角色

--- src/main/java/io/choerodon/iam/app/service/impl/MemberRoleAssignC7nServiceImpl.java
+++ src/main/java/io/choerodon/iam/app/service/impl/MemberRoleAssignC7nServiceImpl.java
@@ -58,45 +58,28 @@ public class MemberRoleAssignC7nServiceImpl extends MemberRoleAssignService {
     }
 
     /**
-     *  原本用于hzero界面操作 同步gitlab角色
-     *  但是已经有了 {@link RoleAssignC7nObserver}
-     *  在下个版本删除
+     * 重写批量移除角色
+     * 不能在hzero界面移除项目层角色
+     * c7n未使用发方法
+     *
+     * @param memberRoleList
+     * @param checkAuth
      */
-//    protected void saveMemberRole(List<MemberRole> memberRoleList) {
-//        super.saveMemberRole(memberRoleList);
-//        // hzero界面分配角色 同步gitlab角色
-//        if (!CollectionUtils.isEmpty(memberRoleList)
-//                && memberRoleList.get(0).getMemberType().equals(MemberType.USER.value())
-//                && (CollectionUtils.isEmpty(memberRoleList.get(0).getAdditionalParams())
-//                || ObjectUtils.isEmpty(memberRoleList.get(0).getAdditionalParams().get(MemberRoleConstants.MEMBER_TYPE))
-//                || !MemberRoleConstants.MEMBER_TYPE_CHOERODON.equals(memberRoleList.get(0).getAdditionalParams().get(MemberRoleConstants.MEMBER_TYPE)))) {
-//            Map<Long, List<MemberRole>> listMap = memberRoleList.stream().collect(Collectors.groupingBy(MemberRole::getMemberId));
-//            List<UserMemberEventPayload> userMemberEventPayloads = new ArrayList<>();
-//            Long organizationId = 0L;
-//            for (Long memberId : listMap.keySet()) {
-//                List<MemberRole> memberRoles = listMap.get(memberId);
-//                if (!CollectionUtils.isEmpty(memberRoles)) {
-//                    Set<Long> roleIds = memberRoles.stream().map(MemberRole::getRoleId).collect(Collectors.toSet());
-//                    Set<String> labelNames = labelC7nMapper.selectLabelNamesInRoleIds(roleIds);
-//                    if (!CollectionUtils.isEmpty(roleIds)
-//                            && labelNames.contains(RoleLabelEnum.TENANT_ROLE.value())) {
-//                        organizationId = memberRoles.get(0).getSourceId();
-//                        UserMemberEventPayload userMemberEventPayload = new UserMemberEventPayload();
-//                        userMemberEventPayload.setUserId(memberId);
-//                        userMemberEventPayload.setRoleLabels(labelNames);
-//                        userMemberEventPayload.setResourceId(organizationId);
-//                        userMemberEventPayload.setResourceType(ResourceLevel.ORGANIZATION.value());
-//                        userMemberEventPayloads.add(userMemberEventPayload);
-//                    }
-//                }
-//            }
-//            if (!CollectionUtils.isEmpty(userMemberEventPayloads)) {
-//                Long userId = 0L;
-//                if (DetailsHelper.getUserDetails() != null) {
-//                    userId = DetailsHelper.getUserDetails().getUserId();
-//                }
-//                roleMemberService.updateMemberRole(userId, userMemberEventPayloads, ResourceLevel.ORGANIZATION, organizationId);
-//            }
-//        }
-//    }
+    @Override
+    public void revokeMemberRole(List<MemberRole> memberRoleList, boolean checkAuth) {
+        if (CollectionUtils.isEmpty(memberRoleList)) {
+            return;
+        }
+        for (MemberRole memberRole : memberRoleList) {
+            List<String> labelList = roleC7nMapper.listRoleLabels(memberRole.getRoleId()).stream().map(Label::getName).collect(Collectors.toList());
+            if (labelList.contains(RoleLabelEnum.PROJECT_ROLE.value())
+                    && (CollectionUtils.isEmpty(memberRole.getAdditionalParams())
+                    || ObjectUtils.isEmpty(memberRole.getAdditionalParams().get(MemberRoleConstants.MEMBER_TYPE))
+                    || !MemberRoleConstants.MEMBER_TYPE_CHOERODON.equals(memberRole.getAdditionalParams().get(MemberRoleConstants.MEMBER_TYPE)))) {
+                throw new CommonException("error.role.type");
+            }
+        }
+        super.revokeMemberRole(memberRoleList, checkAuth);
+    }
+
 }

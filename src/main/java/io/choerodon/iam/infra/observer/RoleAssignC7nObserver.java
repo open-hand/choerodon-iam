aef5940c5d1a9335f7e3e5c6db2a1905709d2d40
devil-scp <changping.shi@hand-china.com>
2020-11-21 00:30:31 +0800

    [IMP] 修改批量同步角色

--- src/main/java/io/choerodon/iam/infra/observer/RoleAssignC7nObserver.java
+++ src/main/java/io/choerodon/iam/infra/observer/RoleAssignC7nObserver.java
@@ -5,7 +5,6 @@ import java.util.stream.Collectors;
 
 import org.hzero.iam.domain.entity.Label;
 import org.hzero.iam.domain.entity.MemberRole;
-import org.hzero.iam.domain.entity.User;
 import org.hzero.iam.domain.service.role.observer.RoleAssignObserver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
@@ -15,15 +14,12 @@ import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
 
 import io.choerodon.core.iam.ResourceLevel;
-import io.choerodon.core.oauth.DetailsHelper;
 import io.choerodon.iam.app.service.ProjectUserService;
 import io.choerodon.iam.app.service.RoleMemberService;
-import io.choerodon.iam.infra.asserts.UserAssertHelper;
 import io.choerodon.iam.infra.constant.MemberRoleConstants;
 import io.choerodon.iam.infra.dto.LabelDTO;
 import io.choerodon.iam.infra.dto.payload.UserMemberEventPayload;
 import io.choerodon.iam.infra.enums.MemberType;
-import io.choerodon.iam.infra.enums.RoleLabelEnum;
 import io.choerodon.iam.infra.mapper.LabelC7nMapper;
 
 /**
@@ -44,25 +40,38 @@ public class RoleAssignC7nObserver implements RoleAssignObserver {
     @Override
     public void assignMemberRole(List<MemberRole> memberRoleList) {
         if (isHzeroMemberRole(memberRoleList)) {
-            Map<Long, Set<String>> userRoleLabelsMap = getUserRoleLabelsMap(memberRoleList);
-            projectUserService.assignUsersProjectRolesEvent(memberRoleList.get(0).getSourceId(), ResourceLevel.ORGANIZATION, userRoleLabelsMap);
+            Map<Long, List<MemberRole>> userMemberMap = memberRoleList.stream().collect(Collectors.groupingBy(MemberRole::getMemberId));
+            userMemberMap.forEach((userId, userMemberList) -> {
+                Map<Long, List<MemberRole>> sourceMemberMap = userMemberList.stream().collect(Collectors.groupingBy(MemberRole::getSourceId));
+                sourceMemberMap.forEach((sourceId, sourceMemberList) -> {
+                    Map<Long, Set<String>> userRoleLabelsMap = getUserRoleLabelsMap(sourceMemberList);
+                    projectUserService.assignUsersProjectRolesEvent(sourceId, ResourceLevel.ORGANIZATION, userRoleLabelsMap);
+                });
+            });
         }
     }
 
     @Override
     public void revokeMemberRole(List<MemberRole> memberRoleList) {
         if (isHzeroMemberRole(memberRoleList)) {
-            Map<Long, Set<String>> userRoleLabelsMap = getUserRoleLabelsMap(memberRoleList);
-            userRoleLabelsMap.forEach((userId, labels) -> {
-                List<UserMemberEventPayload> userMemberEventPayloads = new ArrayList<>();
-                UserMemberEventPayload userMemberEventMsg = new UserMemberEventPayload();
-                userMemberEventMsg.setResourceId(memberRoleList.get(0).getSourceId());
-                userMemberEventMsg.setResourceType(ResourceLevel.ORGANIZATION.value());
-                userMemberEventMsg.setUserId(userId);
-                userMemberEventMsg.setRoleLabels(labels);
-                userMemberEventPayloads.add(userMemberEventMsg);
-                roleMemberService.deleteMemberRoleForSaga(userId, userMemberEventPayloads, ResourceLevel.ORGANIZATION, memberRoleList.get(0).getSourceId());
+            Map<Long, List<MemberRole>> userMemberMap = memberRoleList.stream().collect(Collectors.groupingBy(MemberRole::getMemberId));
+            userMemberMap.forEach((userId, userMemberList) -> {
+                Map<Long, List<MemberRole>> sourceMemberMap = userMemberList.stream().collect(Collectors.groupingBy(MemberRole::getSourceId));
+                sourceMemberMap.forEach((sourceId, sourceMemberList) -> {
+                    Map<Long, Set<String>> userRoleLabelsMap = getUserRoleLabelsMap(sourceMemberList);
+                    userRoleLabelsMap.forEach((uid, labels) -> {
+                        List<UserMemberEventPayload> userMemberEventPayloads = new ArrayList<>();
+                        UserMemberEventPayload userMemberEventMsg = new UserMemberEventPayload();
+                        userMemberEventMsg.setResourceId(memberRoleList.get(0).getSourceId());
+                        userMemberEventMsg.setResourceType(ResourceLevel.ORGANIZATION.value());
+                        userMemberEventMsg.setUserId(uid);
+                        userMemberEventMsg.setRoleLabels(labels);
+                        userMemberEventPayloads.add(userMemberEventMsg);
+                        roleMemberService.deleteMemberRoleForSaga(uid, userMemberEventPayloads, ResourceLevel.ORGANIZATION, sourceId);
+                    });
+                });
             });
+
         }
     }
 

d4a9e7b4f2e3f673a2adc91bb14856bb047256b5
devil-scp <changping.shi@hand-china.com>
2020-11-21 12:02:43 +0800

    [IMP] 修改设置角色参数

--- src/main/java/io/choerodon/iam/app/service/impl/MemberRoleAssignC7nServiceImpl.java
+++ src/main/java/io/choerodon/iam/app/service/impl/MemberRoleAssignC7nServiceImpl.java
@@ -47,7 +47,11 @@ public class MemberRoleAssignC7nServiceImpl extends MemberRoleAssignService {
             List<Long> oldTenantRoleIds = memberRoleC7nMapper.listRoleByUserIdAndLevel(memberId, ResourceLevel.ORGANIZATION.value()).stream().map(Role::getId).collect(Collectors.toList());
             for (MemberRole memberRole : listMap.get(memberId)) {
                 if (oldTenantRoleIds.contains(memberRole.getRoleId())) {
-                    memberRole.getAdditionalParams().putAll(additionalParams);
+                    if (memberRole.getAdditionalParams() == null) {
+                        memberRole.setAdditionalParams(additionalParams);
+                    } else {
+                        memberRole.getAdditionalParams().putAll(additionalParams);
+                    }
                     continue;
                 }
                 List<String> labelList = roleC7nMapper.listRoleLabels(memberRole.getRoleId()).stream().map(Label::getName).collect(Collectors.toList());

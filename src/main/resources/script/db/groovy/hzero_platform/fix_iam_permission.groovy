0169ac095cda390e2b5a4ae9cd380870c23b9752
devil-scp <changping.shi@hand-china.com>
2020-11-24 11:53:22 +0800

    [IMP] 修改更新groovy文件

--- src/main/resources/script/db/groovy/hzero_platform/fix_iam_permission.groovy
+++ src/main/resources/script/db/groovy/hzero_platform/fix_iam_permission.groovy
@@ -2,6 +2,6 @@ package script.db
 
 databaseChangeLog(logicalFilePath: 'script/db/fix_iam_permission.groovy') {
     changeSet(author: 'scp', id: '2020-11-24-iam-permission-modify-column') {
-        sql("ALTER TABLE iam_permission MODIFY COLUMN `path` VARCHAR(256) BINARY commant '权限对应的api路径'")
+        sql("ALTER TABLE iam_permission MODIFY COLUMN `path` VARCHAR(256) BINARY comment '权限对应的api路径'")
     }
 }

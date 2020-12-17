cc00090a60bbf59dfd870c8c853f34ac6607713b
devil-scp <changping.shi@hand-china.com>
2020-12-17 12:01:48 +0800

    [ADD] 添加修复脚本

--- src/main/resources/script/db/groovy/hzero_platform/fix_iam_permission.groovy
+++ src/main/resources/script/db/groovy/hzero_platform/fix_iam_permission.groovy
@@ -4,4 +4,8 @@ databaseChangeLog(logicalFilePath: 'script/db/fix_iam_permission.groovy') {
     changeSet(author: 'scp', id: '2020-11-24-iam-permission-modify-column') {
         sql("ALTER TABLE iam_permission MODIFY COLUMN `path` VARCHAR(256) BINARY comment '权限对应的api路径'")
     }
+
+    changeSet(author: 'scp', id: '2020-12-17-iam-user') {
+        sql("UPDATE iam_user SET phone = CONCAT(phone, '*#*') WHERE is_enabled = 0")
+    }
 }

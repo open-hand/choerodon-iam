67f7515478c6b055d9eeeaf51e82932dc69990fc
王灏 <2507172753@qq.com>
2020-12-30 09:50:51 +0800

    [IMP] 修改初始化租户逻辑

--- src/main/java/io/choerodon/iam/app/service/impl/EnterpriseInfoServiceImpl.java
+++ src/main/java/io/choerodon/iam/app/service/impl/EnterpriseInfoServiceImpl.java
@@ -1,16 +1,13 @@
 package io.choerodon.iam.app.service.impl;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import okhttp3.ResponseBody;
 import org.hzero.iam.domain.entity.Tenant;
-import org.hzero.iam.domain.entity.TenantConfig;
 import org.hzero.iam.domain.entity.User;
 import org.hzero.iam.infra.mapper.TenantMapper;
 import org.hzero.iam.infra.mapper.UserMapper;
-import org.hzero.iam.saas.app.service.TenantService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,16 +18,14 @@ import org.springframework.util.CollectionUtils;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 
-import io.choerodon.asgard.saga.producer.TransactionalProducer;
 import io.choerodon.core.exception.CommonException;
 import io.choerodon.core.oauth.CustomUserDetails;
 import io.choerodon.core.oauth.DetailsHelper;
 import io.choerodon.iam.api.vo.EnterpriseInfoVO;
 import io.choerodon.iam.app.service.EnterpriseInfoService;
-import io.choerodon.iam.app.service.TimeZoneWorkCalendarService;
+import io.choerodon.iam.app.service.TenantC7nService;
 import io.choerodon.iam.infra.constant.TenantConstants;
 import io.choerodon.iam.infra.dto.EnterpriseInfoDTO;
-import io.choerodon.iam.infra.enums.TenantConfigEnum;
 import io.choerodon.iam.infra.mapper.EnterpriseInfoMapper;
 import io.choerodon.iam.infra.retrofit.IamClient;
 import io.choerodon.iam.infra.retrofit.RetrofitHandler;
@@ -62,11 +57,8 @@ public class EnterpriseInfoServiceImpl implements EnterpriseInfoService {
     @Autowired
     private UserMapper userMapper;
     @Autowired
-    private TenantService tenantService;
-    @Autowired
-    private TimeZoneWorkCalendarService timeZoneWorkCalendarService;
-    @Autowired
-    private TransactionalProducer producer;
+    private TenantC7nService tenantC7nService;
+
 
 
     @Override
@@ -93,10 +85,7 @@ public class EnterpriseInfoServiceImpl implements EnterpriseInfoService {
 
         if (tenant == null) {
             // 默认组织不存在则创建
-            Tenant defaultTenant = new Tenant();
-            defaultTenant.setTenantName(enterpriseInfoVO.getOrganizationName());
-            defaultTenant.setTenantNum(enterpriseInfoVO.getTenantNum());
-            createDefaultTenant(defaultTenant);
+            tenantC7nService.createDefaultTenant(enterpriseInfoVO.getOrganizationName(), enterpriseInfoVO.getTenantNum());
         } else {
             // 默认组织已经创建则只更新组织名
             Tenant newTenant = new Tenant();
@@ -133,39 +122,6 @@ public class EnterpriseInfoServiceImpl implements EnterpriseInfoService {
         } catch (IOException e) {
             LOGGER.info("::Retrofit::An exception occurred during execution:{}", e);
         }
-    }
-
-
-    private void createDefaultTenant(Tenant defaultTenant) {
-        defaultTenant.setEnabledFlag(1);
-        initConfig(defaultTenant);
-        tenantService.createTenant(defaultTenant);
-        timeZoneWorkCalendarService.handleOrganizationInitTimeZone(defaultTenant.getTenantId());
-    }
-
-    private void initConfig(Tenant defaultTenant) {
-        List<TenantConfig> tenantConfigs = new ArrayList<>();
-
-        TenantConfig userId = new TenantConfig();
-        userId.setConfigKey(TenantConfigEnum.USER_ID.value());
-        userId.setConfigValue(String.valueOf(1L));
-        tenantConfigs.add(userId);
-
-        TenantConfig register = new TenantConfig();
-        register.setConfigValue("false");
-        register.setConfigKey(TenantConfigEnum.IS_REGISTER.value());
-        tenantConfigs.add(register);
-
-        TenantConfig category = new TenantConfig();
-        category.setConfigKey(TenantConfigEnum.CATEGORY.value());
-        category.setConfigValue(TenantConstants.DEFAULT_CATEGORY);
-        tenantConfigs.add(category);
-
-        TenantConfig token = new TenantConfig();
-        token.setConfigKey(TenantConfigEnum.REMOTE_TOKEN_ENABLED.value());
-        token.setConfigValue("true");
-        tenantConfigs.add(token);
-        defaultTenant.setTenantConfigs(tenantConfigs);
     }
 
 }

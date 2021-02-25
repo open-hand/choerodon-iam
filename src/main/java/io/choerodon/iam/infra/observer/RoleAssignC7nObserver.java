c188d0106e34cf6898f9f1587e3270c3c9769feb
18986 <changping.shi@hand-chian.com>
2021-02-25 21:18:14 +0800

    [IMP] 添加hzero界面分配角色消息发送

--- src/main/java/io/choerodon/iam/infra/observer/RoleAssignC7nObserver.java
+++ src/main/java/io/choerodon/iam/infra/observer/RoleAssignC7nObserver.java
@@ -3,11 +3,13 @@ package io.choerodon.iam.infra.observer;
 import java.util.*;
 import java.util.stream.Collectors;
 
-import org.hzero.iam.domain.entity.Label;
-import org.hzero.iam.domain.entity.MemberRole;
-import org.hzero.iam.domain.entity.Role;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hzero.iam.domain.entity.*;
 import org.hzero.iam.domain.service.role.observer.RoleAssignObserver;
 import org.hzero.iam.infra.mapper.RoleMapper;
+import org.hzero.iam.infra.mapper.TenantMapper;
+import org.hzero.iam.infra.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
@@ -16,6 +18,8 @@ import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
 
 import io.choerodon.core.iam.ResourceLevel;
+import io.choerodon.core.oauth.DetailsHelper;
+import io.choerodon.iam.app.service.MessageSendService;
 import io.choerodon.iam.app.service.RoleMemberService;
 import io.choerodon.iam.infra.constant.MemberRoleConstants;
 import io.choerodon.iam.infra.dto.LabelDTO;
@@ -29,6 +33,8 @@ import io.choerodon.iam.infra.mapper.LabelC7nMapper;
  */
 @Component
 public class RoleAssignC7nObserver implements RoleAssignObserver {
+    private final Log logger = LogFactory.getLog(getClass());
+
     @Autowired
     private LabelC7nMapper labelC7nMapper;
     @Autowired
@@ -36,9 +42,17 @@ public class RoleAssignC7nObserver implements RoleAssignObserver {
     private RoleMemberService roleMemberService;
     @Autowired
     private RoleMapper roleMapper;
+    @Autowired
+    private MessageSendService messageSendService;
+    @Autowired
+    private TenantMapper tenantMapper;
+    @Autowired
+    private UserMapper userMapper;
+
 
     @Override
     public void assignMemberRole(List<MemberRole> memberRoleList) {
+        Long operatorId = DetailsHelper.getUserDetails() == null ? 0L : DetailsHelper.getUserDetails().getUserId();
         if (isHzeroMemberRole(memberRoleList)) {
             if (!CollectionUtils.isEmpty(memberRoleList)) {
                 Map<Long, List<MemberRole>> sourceMemberMap = memberRoleList.stream().collect(Collectors.groupingBy(MemberRole::getSourceId));
@@ -46,8 +60,11 @@ public class RoleAssignC7nObserver implements RoleAssignObserver {
                     Map<Long, Set<String>> userRoleLabelsMap = getUserRoleLabelsMap(sourceMemberList);
                     Map<Long, Set<String>> userOldRoleLabelsMap = getUserOldRoleLabelsMap(sourceMemberList);
                     assignUsersRolesEvent(sourceId, ResourceLevel.ORGANIZATION, userRoleLabelsMap, userOldRoleLabelsMap);
+                    sendTenantAssignMessage(sourceId, sourceMemberList, operatorId);
                 });
             }
+        } else if (isHzeroMemberSiteRole(memberRoleList)) {
+            sendSiteAssignMessage(memberRoleList);
         }
     }
 
@@ -82,6 +99,15 @@ public class RoleAssignC7nObserver implements RoleAssignObserver {
                 || !MemberRoleConstants.MEMBER_TYPE_CHOERODON.equals(objectMap.get(MemberRoleConstants.MEMBER_TYPE)))
                 && (memberRoleList.get(0).getMemberType() == null || memberRoleList.get(0).getMemberType().equals(MemberType.USER.value()))
                 && (StringUtils.isEmpty(memberRoleList.get(0).getSourceType()) || memberRoleList.get(0).getSourceType().contains(ResourceLevel.ORGANIZATION.value()));
+    }
+
+    private Boolean isHzeroMemberSiteRole(List<MemberRole> memberRoleList) {
+        Map<String, Object> objectMap = memberRoleList.get(0).getAdditionalParams();
+        return (CollectionUtils.isEmpty(objectMap)
+                || ObjectUtils.isEmpty(objectMap.get(MemberRoleConstants.MEMBER_TYPE))
+                || !MemberRoleConstants.MEMBER_TYPE_CHOERODON.equals(objectMap.get(MemberRoleConstants.MEMBER_TYPE)))
+                && (memberRoleList.get(0).getMemberType() == null || memberRoleList.get(0).getMemberType().equals(MemberType.USER.value()))
+                && (StringUtils.isEmpty(memberRoleList.get(0).getSourceType()) || memberRoleList.get(0).getSourceType().contains(ResourceLevel.SITE.value()));
     }
 
     private Map<Long, Set<String>> getUserRoleLabelsMap(List<MemberRole> memberRoleList) {
@@ -137,6 +163,28 @@ public class RoleAssignC7nObserver implements RoleAssignObserver {
         }
     }
 
+    private void sendTenantAssignMessage(Long sourceId, List<MemberRole> sourceMemberList, Long operatorId) {
+        try {
+            Tenant tenant = tenantMapper.selectByPrimaryKey(sourceId);
+            sourceMemberList.forEach(memberRole -> {
+                User user = userMapper.selectByPrimaryKey(memberRole.getMemberId());
+                ArrayList<User> list = new ArrayList<>();
+                list.add(user);
+                messageSendService.sendAddMemberMsg(tenant, memberRole.getRoleName(), list, operatorId);
+            });
+        } catch (Exception e) {
+            logger.error("Failed to send message while hzero tenant assign role : ", e);
+        }
+    }
+
+    private void sendSiteAssignMessage(List<MemberRole> memberRoleList) {
+        try {
+            User user = userMapper.selectByPrimaryKey(memberRoleList.get(0).getMemberId());
+            messageSendService.sendSiteAddUserMsg(user, memberRoleList.get(0).getRoleName());
+        } catch (Exception e) {
+            logger.error("Failed to send message while hzero site assign role : ", e);
+        }
+    }
 
     private <T> Set<T> castList(Object obj, Class<T> clazz) {
         HashSet<T> result = new HashSet<T>();

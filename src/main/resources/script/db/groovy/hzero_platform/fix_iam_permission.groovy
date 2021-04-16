654b5a1265e8cd377386334bf49f2bb6f576555d
18986 <changping.shi@hand-chian.com>
2021-04-16 11:12:13 +0800

    [IMP] 修改用户langage字段不能为空

--- src/main/resources/script/db/groovy/hzero_platform/fix_iam_permission.groovy
+++ src/main/resources/script/db/groovy/hzero_platform/fix_iam_permission.groovy
@@ -13,7 +13,7 @@ databaseChangeLog(logicalFilePath: 'script/db/fix_iam_permission.groovy') {
         sql("DELETE FROM iam_label WHERE NAME = 'SITE_MGR' OR NAME = 'TENANT_MGR'")
     }
 
-    changeSet(author: 'scp', id: '2021-03-26-modify-table') {
-        sql("alter table iam_user modify language varchar(16) null;")
+    changeSet(author: 'scp', id: '2021-04-16-modify-table') {
+        sql("alter table iam_user modify language varchar(16);")
     }
 }

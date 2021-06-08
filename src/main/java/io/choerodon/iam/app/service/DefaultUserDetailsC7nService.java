3e61b1bd0b486b86fd094f5ea7da3dd315a2f0c5
18986 <changping.shi@hand-chian.com>
2021-06-08 11:09:14 +0800

    [IMP] 修改切换组织逻辑

--- src/main/java/io/choerodon/iam/app/service/DefaultUserDetailsC7nService.java
+++ src/main/java/io/choerodon/iam/app/service/DefaultUserDetailsC7nService.java
@@ -1,12 +1,15 @@
 package io.choerodon.iam.app.service;
 
 import org.hzero.iam.domain.entity.Tenant;
+import org.hzero.iam.domain.service.RootUserService;
 import org.hzero.iam.domain.service.user.impl.DefaultUserDetailsService;
 import org.hzero.iam.infra.mapper.TenantMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import io.choerodon.core.exception.CommonException;
+import io.choerodon.core.oauth.CustomUserDetails;
+import io.choerodon.core.oauth.DetailsHelper;
 
 /**
  * @Author: scp
@@ -18,13 +21,18 @@ import io.choerodon.core.exception.CommonException;
 public class DefaultUserDetailsC7nService extends DefaultUserDetailsService {
     @Autowired
     private TenantMapper tenantMapper;
+    @Autowired
+    private UserC7nService userC7nService;
 
     @Override
     public void storeUserTenant(Long tenantId) {
         if (tenantId != null) {
-            Tenant tenant = tenantMapper.selectByPrimaryKey(tenantId);
-            if (tenant.getEnabledFlag() != null && tenant.getEnabledFlag() != 1) {
-                throw new CommonException("error.tenant.enable");
+            CustomUserDetails self = DetailsHelper.getUserDetails();
+            if (!RootUserService.isRootUser() && !userC7nService.checkIsOrgRoot(tenantId, self.getUserId())) {
+                Tenant tenant = tenantMapper.selectByPrimaryKey(tenantId);
+                if (tenant.getEnabledFlag() != null && tenant.getEnabledFlag() != 1) {
+                    throw new CommonException("error.tenant.enable");
+                }
             }
             super.storeUserTenant(tenantId);
         }

a08d6f2d1e644b6b57d9311ada06ce4327e56362
张建 <jian.zhang02@hand-china.com>
2021-07-09 15:48:06 +0800

    feat: 需改groovy脚本

--- src/main/resources/script/db/groovy/hzero_platform/fd_dashboard.groovy
+++ src/main/resources/script/db/groovy/hzero_platform/fd_dashboard.groovy
@@ -1,26 +1,16 @@
 package script.db
 databaseChangeLog(logicalFilePath: 'script/db/fd_dashboard.groovy') {
-    def weight_c = 1
-    if(helper.isOracle()){
-    weight_c = 2
-    }
-    if(helper.isOracle()){
-    weight_c = 3
-    }
     changeSet(author: "jian.zhang02@hand-china.com", id: "fd_dashboard-2021-07-07-version-1"){
-        if(helper.dbType().isSupportSequence()){
-            createSequence(sequenceName: 'fd_dashboard_s', startValue:"1")
-        }
         createTable(tableName: "fd_dashboard", remarks: "") {
-            column(name: "dashboard_id", type: "bigint",autoIncrement: true,    remarks: "面板ID")  {constraints(primaryKey: true)} 
-            column(name: "dashboard_type", type: "varchar(" + 30* weight_c + ")",   defaultValue:"CUSTOMIZE",   remarks: "面板类型(CUSTOMIZE/自定义;INTERNAL/内置)")  {constraints(nullable:"false")}  
-            column(name: "dashboard_name", type: "varchar(" + 480* weight_c + ")",  remarks: "面板名称")  {constraints(nullable:"false")}  
-            column(name: "default_flag", type: "tinyint",   defaultValue:"0",   remarks: "默认面板")  {constraints(nullable:"false")}  
-            column(name: "created_by", type: "bigint",   defaultValue:"-1",   remarks: "创建人")  {constraints(nullable:"false")}  
-            column(name: "last_updated_by", type: "bigint",   defaultValue:"-1",   remarks: "最近更新人")  {constraints(nullable:"false")}  
-            column(name: "creation_date", type: "datetime",   defaultValueComputed :"CURRENT_TIMESTAMP",   remarks: "创建时间")  {constraints(nullable:"false")}  
-            column(name: "last_update_date", type: "datetime",   defaultValueComputed :"CURRENT_TIMESTAMP",   remarks: "最近更新时间")  {constraints(nullable:"false")}  
-            column(name: "object_version_number", type: "bigint",   defaultValue:"1",   remarks: "行版本号，用来处理锁")  {constraints(nullable:"false")}  
+            column(name: "dashboard_id", type: "BIGINT(20)",autoIncrement: true,    remarks: "面板ID")  {constraints(primaryKey: true)} 
+            column(name: "dashboard_type", type: "VARCHAR(30)",   defaultValue:"CUSTOMIZE",   remarks: "面板类型(CUSTOMIZE/自定义;INTERNAL/内置)")  {constraints(nullable:"false")}  
+            column(name: "dashboard_name", type: "VARCHAR(480)",  remarks: "面板名称")  {constraints(nullable:"false")}  
+            column(name: "default_flag", type: "TINYINT(1)",   defaultValue:"0",   remarks: "默认面板")  {constraints(nullable:"false")}  
+            column(name: "created_by", type: "BIGINT(20)",   defaultValue:"-1",   remarks: "创建人")  {constraints(nullable:"false")}  
+            column(name: "last_updated_by", type: "BIGINT(20)",   defaultValue:"-1",   remarks: "最近更新人")  {constraints(nullable:"false")}  
+            column(name: "creation_date", type: "DATETIME",   defaultValue:"CURRENT_TIMESTAMP",   remarks: "创建时间")  {constraints(nullable:"false")}  
+            column(name: "last_update_date", type: "DATETIME",   defaultValue:"CURRENT_TIMESTAMP",   remarks: "最近更新时间")  {constraints(nullable:"false")}  
+            column(name: "object_version_number", type: "BIGINT(20)",   defaultValue:"1",   remarks: "行版本号，用来处理锁")  {constraints(nullable:"false")}  
         }
        createIndex(tableName: "fd_dashboard", indexName: "fd_dashboard_n1") {
            column(name: "dashboard_type")
--- src/main/resources/script/db/groovy/hzero_platform/fd_dashboard_user.groovy
+++ src/main/resources/script/db/groovy/hzero_platform/fd_dashboard_user.groovy
@@ -1,25 +1,15 @@
 package script.db
 databaseChangeLog(logicalFilePath: 'script/db/fd_dashboard_user.groovy') {
-    def weight_c = 1
-    if(helper.isOracle()){
-    weight_c = 2
-    }
-    if(helper.isOracle()){
-    weight_c = 3
-    }
     changeSet(author: "jian.zhang02@hand-china.com", id: "fd_dashboard_user-2021-07-07-version-1"){
-        if(helper.dbType().isSupportSequence()){
-            createSequence(sequenceName: 'fd_dashboard_user_s', startValue:"1")
-        }
         createTable(tableName: "fd_dashboard_user", remarks: "") {
-            column(name: "dashboard_user_id", type: "bigint",autoIncrement: true,    remarks: "面板用户ID")  {constraints(primaryKey: true)} 
-            column(name: "user_id", type: "bigint",  remarks: "用户ID")  {constraints(nullable:"false")}  
-            column(name: "dashboard_id", type: "bigint",  remarks: "面板ID")  {constraints(nullable:"false")}  
-            column(name: "created_by", type: "bigint",   defaultValue:"-1",   remarks: "创建人")  {constraints(nullable:"false")}  
-            column(name: "last_updated_by", type: "bigint",   defaultValue:"-1",   remarks: "最近更新人")  {constraints(nullable:"false")}  
-            column(name: "creation_date", type: "datetime",   defaultValueComputed :"CURRENT_TIMESTAMP",   remarks: "创建时间")  {constraints(nullable:"false")}  
-            column(name: "last_update_date", type: "datetime",   defaultValueComputed :"CURRENT_TIMESTAMP",   remarks: "最近更新时间")  {constraints(nullable:"false")}  
-            column(name: "object_version_number", type: "bigint",   defaultValue:"1",   remarks: "行版本号，用来处理锁")  {constraints(nullable:"false")}  
+            column(name: "dashboard_user_id", type: "BIGINT(20)",autoIncrement: true,    remarks: "面板用户ID")  {constraints(primaryKey: true)} 
+            column(name: "user_id", type: "BIGINT(20)",  remarks: "用户ID")  {constraints(nullable:"false")}  
+            column(name: "dashboard_id", type: "BIGINT(20)",  remarks: "面板ID")  {constraints(nullable:"false")}  
+            column(name: "created_by", type: "BIGINT(20)",   defaultValue:"-1",   remarks: "创建人")  {constraints(nullable:"false")}  
+            column(name: "last_updated_by", type: "BIGINT(20)",   defaultValue:"-1",   remarks: "最近更新人")  {constraints(nullable:"false")}  
+            column(name: "creation_date", type: "DATETIME",   defaultValue:"CURRENT_TIMESTAMP",   remarks: "创建时间")  {constraints(nullable:"false")}  
+            column(name: "last_update_date", type: "DATETIME",   defaultValue:"CURRENT_TIMESTAMP",   remarks: "最近更新时间")  {constraints(nullable:"false")}  
+            column(name: "object_version_number", type: "BIGINT(20)",   defaultValue:"1",   remarks: "行版本号，用来处理锁")  {constraints(nullable:"false")}  
         }
         addUniqueConstraint(columnNames:"dashboard_id,user_id",tableName:"fd_dashboard_user",constraintName: "fd_dashboard_user_u1")
     }

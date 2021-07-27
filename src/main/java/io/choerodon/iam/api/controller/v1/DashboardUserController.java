9139ea336c69a04f324b95816bd2c32136664d5b
张建 <jian.zhang02@hand-china.com>
2021-07-27 16:26:37 +0800

    [FIX] 修改更新视图排序

--- src/main/java/io/choerodon/iam/api/controller/v1/DashboardUserController.java
+++ src/main/java/io/choerodon/iam/api/controller/v1/DashboardUserController.java
@@ -37,7 +37,7 @@ public class DashboardUserController extends BaseController {
     }
 
     @ApiOperation(value = "维护-批量更新用户视图顺序")
-    @Permission(permissionPublic = true)
+    @Permission(level = ResourceLevel.ORGANIZATION, permissionLogin = true)
     @PostMapping("/dashboard-rank")
     public ResponseEntity<List<DashboardUserDTO>> batchUpdateDashboardUserRank(@Encrypt @RequestBody List<DashboardUserDTO> dashboardUserS) {
         return Results.success(dashboardUserService.batchUpdateDashboardUserRank(dashboardUserS));

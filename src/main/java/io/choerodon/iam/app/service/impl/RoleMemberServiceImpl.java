f8f1af2b21608790983632bd287dca66f7d518ba
shanyu <yu.shan@hand-china.com>
2021-08-23 18:30:40 +0800

    [FIX] 市场服务查询逻辑添加角色校验。

--- src/main/java/io/choerodon/iam/app/service/impl/RoleMemberServiceImpl.java
+++ src/main/java/io/choerodon/iam/app/service/impl/RoleMemberServiceImpl.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 
 import com.alibaba.fastjson.JSON;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.choerodon.iam.infra.utils.SagaTopic;
 import org.apache.commons.lang3.BooleanUtils;
 import org.hzero.boot.message.MessageClient;
 import org.hzero.boot.message.entity.MessageSender;
@@ -897,6 +898,13 @@ public class RoleMemberServiceImpl implements RoleMemberService {
 
     @Override
     public Boolean checkRole(Long userId) {
-        return memberRoleC7nMapper.checkRole(userId) > 0;
+        boolean returnFlag = false;
+        User requestUser = new User();
+        requestUser.setId(userId);
+        User user = userMapper.selectByPrimaryKey(requestUser);
+        if ((!ObjectUtils.isEmpty(user) && user.getAdmin().equals(true)) || memberRoleC7nMapper.checkRole(userId) > 0) {
+            returnFlag = true;
+        }
+        return returnFlag;
     }
 }

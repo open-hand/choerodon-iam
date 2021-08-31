f25226f1962e1c4febe4b44ea83dec1f6c7b48e8
wangxiang <xiangwang04@hand-china.com>
2021-08-31 21:19:29 +0800

    [IMP] 修改字段

--- src/main/java/io/choerodon/iam/app/service/impl/UserC7nServiceImpl.java
+++ src/main/java/io/choerodon/iam/app/service/impl/UserC7nServiceImpl.java
@@ -1375,6 +1375,9 @@ public class UserC7nServiceImpl implements UserC7nService {
         CustomUserDetails customUserDetails = DetailsHelperAssert.userDetailNotExisted();
         Long userId = customUserDetails.getUserId();
         UserDTO userDTO = userC7nMapper.queryPersonalInfo(userId);
+        if (StringUtils.isEmpty(userDTO.getPhone())){
+            userDTO.setPhoneCheckFlag(BaseConstants.Flag.YES);
+        }
         return userDTO;
     }
 

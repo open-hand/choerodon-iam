01107afcc69c8c12880faf878aaa2e395ad1ea26
wangxiang <xiangwang04@hand-china.com>
2021-10-09 10:46:52 +0800

    [FIX] 增加邮件接收者

--- src/main/java/io/choerodon/iam/app/service/impl/MessageSendServiceImpl.java
+++ src/main/java/io/choerodon/iam/app/service/impl/MessageSendServiceImpl.java
@@ -499,6 +499,7 @@ public class MessageSendServiceImpl implements MessageSendService {
             //发送组织层和项目层消息时必填 当前组织id
             objectMap.put(MessageAdditionalType.PARAM_TENANT_ID.getTypeName(), dbUser.getOrganizationId());
 
+            messageSender.setReceiverAddressList(receiverList);
             messageSender.setAdditionalInformation(objectMap);
             messageClient.async().sendMessage(messageSender);
         } catch (Exception e) {

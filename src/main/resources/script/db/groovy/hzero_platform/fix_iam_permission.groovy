c9adcd8b41e79102760c264e30eedc2a01471bb3
18986 <changping.shi@hand-chian.com>
2021-10-09 11:10:58 +0800

    [IMP] 修改client重定向地址长度限制

--- src/main/resources/script/db/groovy/hzero_platform/fix_iam_permission.groovy
+++ src/main/resources/script/db/groovy/hzero_platform/fix_iam_permission.groovy
@@ -24,4 +24,8 @@ databaseChangeLog(logicalFilePath: 'script/db/fix_iam_permission.groovy') {
     changeSet(author: 'scp', id: '2021-05-08-iam-user') {
         sql("UPDATE iam_user SET id=0 where login_name='ANONYMOUS'")
     }
+
+    changeSet(author: 'scp', id: '2021-10-09-iam-client-modify-column') {
+        sql("ALTER TABLE oauth_client MODIFY COLUMN `web_server_redirect_uri` text BINARY comment '授权重定向URL'")
+    }
 }

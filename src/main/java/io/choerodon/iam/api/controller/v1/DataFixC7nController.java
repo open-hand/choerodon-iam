c7f7f1465befae003123d4ccb52d0c12249947bc
18986 <changping.shi@hand-chian.com>
2021-11-24 14:47:51 +0800

    [ADD] 添加修复组织多语言逻辑

--- src/main/java/io/choerodon/iam/api/controller/v1/DataFixC7nController.java
+++ src/main/java/io/choerodon/iam/api/controller/v1/DataFixC7nController.java
@@ -1,42 +1,31 @@
 package io.choerodon.iam.api.controller.v1;
 
-import java.util.List;
-
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.hzero.core.util.Results;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.choerodon.core.iam.ResourceLevel;
-import io.choerodon.iam.app.service.ProjectC7nService;
+import io.choerodon.iam.app.service.TenantC7nService;
 import io.choerodon.iam.infra.config.C7nSwaggerApiConfig;
-import io.choerodon.iam.infra.dto.ProjectDTO;
 import io.choerodon.swagger.annotation.Permission;
 
 @Api(tags = C7nSwaggerApiConfig.CHOERODON_DATA_FIX)
 @RestController
 @RequestMapping(value = "/choerodon/v1/fix")
 public class DataFixC7nController {
-    private ProjectC7nService projectService;
+    @Autowired
+    private TenantC7nService tenantC7nService;
 
-    public DataFixC7nController(ProjectC7nService projectService) {
-        this.projectService = projectService;
-    }
-
-//    @Permission(type = ResourceType.SITE, roles = {InitRoleCode.SITE_ADMINISTRATOR})
-//    @ApiOperation(value = "分页查询所有组织基本信息（修数据用）")
-//    @GetMapping(value = "/organizations/all")
-//    @CustomPageRequest
-//    public ResponseEntity<List<OrganizationSimplifyDTO>> getAllOrgsList() {
-//        return new ResponseEntity<>(dataFixService.getAllOrgsList(), HttpStatus.OK);
-//    }
-
-    @Permission(level = ResourceLevel.SITE, permissionWithin = true)
-    @ApiOperation(value = "查询所有的项目")
-    @GetMapping("/projects/all")
-    public ResponseEntity<List<ProjectDTO>> listAllProjects() {
-        return ResponseEntity.ok(projectService.listAllProjects());
+    @Permission(level = ResourceLevel.SITE)
+    @ApiOperation(value = "同步组织多语言")
+    @GetMapping("/tenant_tl")
+    public ResponseEntity<Void> syncTenantTl() {
+        tenantC7nService.syncTenantTl();
+        return Results.success();
     }
 }

8de7934f9d0c20678700aa82d8a9a4c3434f6107
王灏 <hao.wang08@hand-china.com>
2021-12-06 19:30:53 +0800

    [imp] 新增图表多语言

---
+++ src/main/resources/script/db/groovy/hzero_platform/fd_report_tl.groovy
@@ -0,0 +1,13 @@
+package script.db
+
+databaseChangeLog(logicalFilePath: 'script/db/fd_report_tl.groovy') {
+    changeSet(author: "wanghao", id: "2021-12-06-fd_report_tl") {
+        createTable(tableName: "fd_report_tl", remarks: "") {
+            column(name: "id", type: "bigint",  remarks: "")  {constraints(nullable:"false")}
+            column(name: "lang", type: "varchar(16)",  remarks: "")  {constraints(nullable:"false")}
+            column(name: 'description', type: "varchar(128)", remarks: '描述')
+        }
+
+        addUniqueConstraint(columnNames:"id,lang",tableName:"fd_report_tl",constraintName: "fd_report_tl_u1")
+    }
+}

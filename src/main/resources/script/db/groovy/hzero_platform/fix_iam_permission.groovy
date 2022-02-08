b5e91799ede3e539ff133c8f5b6667a57e9820d9
18986 <changping.shi@hand-chian.com>
2022-02-08 09:47:39 +0800

    [IMP] 修改pom文件

--- src/main/resources/script/db/groovy/hzero_platform/fix_iam_permission.groovy
+++ src/main/resources/script/db/groovy/hzero_platform/fix_iam_permission.groovy
@@ -24,4 +24,11 @@ databaseChangeLog(logicalFilePath: 'script/db/fix_iam_permission.groovy') {
     changeSet(author: 'scp', id: '2021-05-08-iam-user') {
         sql("UPDATE iam_user SET id=0 where login_name='ANONYMOUS'")
     }
+
+    changeSet(id: '2022-01-21-create-index', author: 'changping.shi@hand-china.com') {
+        createIndex(tableName: 'IAM_MENU_PERMISSION', indexName: 'NK_IAM_MENU_PERMISSION_N2', unique: false) {
+            column(name: 'PERMISSION_CODE')
+        }
+    }
+    // 不要随意在这里加changeset 考虑全新初始化表不存在的情况 最好加在z_fix_iam
 }

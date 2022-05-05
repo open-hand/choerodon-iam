a25b94d4f05dc8aafc27824f30891ee93530ecba
18986 <changping.shi@hand-chian.com>
2022-05-05 11:57:05 +0800

    [IMP] 修改接口为异步接口

--- src/main/java/io/choerodon/iam/app/service/impl/RoleC7nServiceImpl.java
+++ src/main/java/io/choerodon/iam/app/service/impl/RoleC7nServiceImpl.java
@@ -21,6 +21,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
@@ -253,6 +254,7 @@ public class RoleC7nServiceImpl implements RoleC7nService {
     }
 
     @Override
+    @Async
     public void syncRolesAndPermission() {
         syncStatusVO.setCompletedStepCount(0);
         syncStatusVO.setAllStepCount(2);

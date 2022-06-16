ba783fb9f9d3c80d90b092185ed85432d2092d92
18986 <changping.shi@hand-chian.com>
2022-06-16 12:31:14 +0800

    [IMP] 修改唯一性约束

--- src/main/resources/script/db/groovy/hzero_platform/z_fix_iam.groovy
+++ src/main/resources/script/db/groovy/hzero_platform/z_fix_iam.groovy
@@ -9,8 +9,4 @@ databaseChangeLog(logicalFilePath: 'script/db/z_fix_iam.groovy') {
     changeSet(author: 'scp', id: '2021-10-09-iam-client-modify-column') {
         sql("ALTER TABLE oauth_client MODIFY COLUMN `web_server_redirect_uri` text BINARY comment '授权重定向URL'")
     }
-
-    changeSet(author: 'scp', id: '2022-06-16-iam-user-add-UniqueConstraint') {
-        addUniqueConstraint(tableName: 'IAM_USER', columnNames: 'email', constraintName: 'UK_IAM_USER_U2')
-    }
 }
